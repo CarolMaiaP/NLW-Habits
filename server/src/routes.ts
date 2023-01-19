a13c6ba17b7dd86fd6d8b260b2e7b28e96b49e7f
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-19 16:06:35 -0300

    feat: reset datetime with dayjs

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,6 +1,7 @@
 import { prisma } from "./lib/prisma"
 import { FastifyInstance } from "fastify"
 import {z} from 'zod'
+import dayjs from "dayjs"
 
 export async function appRoutes(app: FastifyInstance){
   app.post('/habits', async (request) => {
@@ -13,10 +14,12 @@ export async function appRoutes(app: FastifyInstance){
 
     const { title, weekDays } = createHabitBody.parse(request.body)
   
+    const today = dayjs().startOf('day').toDate()
+
     await prisma.habit.create({
       data: {
         title,
-        created_at: new Date(),
+        created_at: today,
         weekDays: {
           create: weekDays.map(weekDay => {
             return {
