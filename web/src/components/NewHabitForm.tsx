4108a8c3010b37d44edf26fb83f7b15188730128
Caroline Maia Abreu da Silva <carolmas99@gmail.com>
2023-01-31 16:46:40 -0300

    styles: transitions added in the components

--- web/src/components/NewHabitForm.tsx
+++ web/src/components/NewHabitForm.tsx
@@ -76,7 +76,7 @@ export function NewHabitForm(){
                 handleToggleWeekDay(index)
               }}
             >
-              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500'>
+              <div className='h-8 w-8 rounded-lg flex items-center justify-center bg-zinc-900 border-2 border-zinc-800 group-data-[state=checked]:bg-green-500 group-data-[state=checked]:border-green-500 transition-colors'>
                 <Checkbox.Indicator>
                   <Check size={20} className="text-white" />
                 </Checkbox.Indicator>
@@ -93,7 +93,7 @@ export function NewHabitForm(){
       <button 
         type="submit" 
         onClick={handleCreateHabit}
-        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500">
+        className="mt-6 rounded-lg p-4 flex items-center justify-center gap-3 font-semibold bg-green-600 hover:bg-green-500 transition-colors">
         <Check size={20} weight="bold" />
         Confirmar
       </button>
